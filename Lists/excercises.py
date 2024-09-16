e4810140e055f0ae0ad0114bb891e07769d152cc
Karina Rashchynskaya <karinaras97@yahoo.com>
2024-09-16 21:15:47 +0300

    feat: create simple functions for lists

--- Lists/excercises.py
+++ Lists/excercises.py
@@ -44,6 +44,26 @@ def find_the_smallest_requested_element(input_list,number):
     return dictionary[number]
 
 
+def pairs_with_sum(lst, g_sum):
+
+    dictionary = {}
+    # Create an empty list 'pairs' to store the pairs that sum up to the target value
+    pairs = []
+    # Iterate through the elements in the input list 'lst'
+    for num in lst:
+        # Calculate the complement of the current number with respect to the target value
+        complement = g_sum - num
+        # Check if the complement is already in 'complement_dict'
+        if complement in dictionary:
+            # If found, add the current number and its complement as a pair to 'pairs'
+            pairs.append((num, complement))
+        else:
+            # If not found, store the complement in 'complement_dict' for future checks
+            dictionary[num] = complement
+    # Return the list of pairs that sum up to the target value
+    return pairs
+
+
 # input_list = list([1,7,3,10,15,3])
 # print("The maximum value is :", get_the_maximum_value(input_list))
 ############################################################################
@@ -63,3 +83,7 @@ print("The kth smallest element in a list is:", find_the_smallest_requested_elem
 
 
 #Write a Python program to find all the pairs in a list whose sum is equal to a given value.
+original_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+number = input("Give a number")
+print("List all pairs of the said list whose sum equals to", number)
+print(pairs_with_sum(original_list,int(number)))
