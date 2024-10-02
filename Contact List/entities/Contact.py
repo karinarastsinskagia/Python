e9bcff009a78fa385b51ee58580447f71353704d
Karina Rashchynskaya <karinaras97@yahoo.com>
2024-10-02 09:44:47 +0300

    feat: create contact relation

--- Contact List/entities/Contact.py
+++ Contact List/entities/Contact.py
@@ -1,19 +1,33 @@
+import mysql.connector
+from docutils.nodes import contact
+
+mydb = mysql.connector.connect(user='root', password='password', host='127.0.0.1', database='python_mysql')
+
+
 class Contact:
 
-    def __init__(self, id,first_name,last_name,number,address):
-        self.id = id
-        self.first_name = first_name
-        self.last_name = last_name
-        self.number = number
-        self.address = number
+    # def __init__(self, id,first_name,last_name,number,address):
+    #     self.id = id
+    #     self.first_name = first_name
+    #     self.last_name = last_name
+    #     self.number = number
+    #     self.address = number
 
-    def add_new_contact(self, id, first_name, last_name, number, address):
+    def add_new_contact(self, user_id:int,first_name:str, last_name:str, number:str, address:str):
+        cursor = mydb.cursor()
+        cursor.execute(f"INSERT INTO contacts (user_id, first_name, last_name, number, address) VALUES ('{user_id}','{first_name}', '{last_name}','{number}', '{address}')")
+        mydb.commit()
 
-        self.id = id
-        self.first_name = first_name
-        self.last_name = last_name
-        self.number = number
-        self.address = number
+        # cursor.execute(f"SELECT * FROM contacts WHERE idcontacts = {cursor.lastrowid}")
+        # rows = cursor.fetchall()
+        #
+        # # Display the results
+        # for row in rows:
+        #     user_id, username = row
+        #     print(f"The new user has id : {user_id} - and full name : {username}")
+
+        cursor.close()
+        return None
 
     def update_contact(self, id, first_name, last_name, number, address):
         self.id = id
@@ -42,3 +56,7 @@ class Contact:
         self.last_name = last_name
         self.number = number
         self.address = number
+
+
+contact = Contact()
+contact.add_new_contact(1,'T.','Ol.','123445','Foo Bar')
--- Contact List/entities/User.py
+++ Contact List/entities/User.py
@@ -1,4 +1,3 @@
-import Contact
 import mysql.connector
 
 mydb = mysql.connector.connect(user='root', password='password', host='127.0.0.1', database='python_mysql')
@@ -18,12 +17,26 @@ def get_all():
 
     return None
 
+def get_users_and_contacts():
+    cursor = mydb.cursor()
+    cursor.execute(f"SELECT users.username, contacts.first_name, contacts.number FROM users JOIN contacts ON users.idusers = contacts.user_id")
+    rows = cursor.fetchall()
+
+    # Display the results
+    for row in rows:
+        username,first_name,number = row
+        print(f"User Name : {username} - Contact Name : {first_name} - Number : {number}")
+    # Close the cursor and connection
+    cursor.close()
+
+    return None
+
 
 class User:
 
     id: int
     name: str
-    contact_list: Contact
+    contact_list: []
 
     # def __init__(self,id:int,name:str):
     #     self.id = id
@@ -50,6 +63,8 @@ class User:
     def create_new_user(self,name: str):
         cursor = mydb.cursor()
         cursor.execute(f"INSERT INTO users (username) VALUES ('{name}')")
+        mydb.commit()
+
         cursor.execute(f"SELECT * FROM users WHERE idusers = {cursor.lastrowid}")
         rows = cursor.fetchall()
 
@@ -61,23 +76,73 @@ class User:
         cursor.close()
         return None
 
-# def update_username(name: str):
-#     return None
+    def update_username(self, userid:int, name: str):
 
-# def delete_user(name: str):
-#     return None
+        cursor = mydb.cursor()
+        cursor.execute(f"UPDATE users SET username= '{name}' WHERE idusers = {userid}")
+        mydb.commit()
+        cursor.close()
 
-# def get_users_contact_list():
-#     return User.contact_list
+        return None
+
+    def delete_user(self, userid:int):
+
+        cursor = mydb.cursor()
+        cursor.execute(f"DELETE FROM users WHERE idusers = {userid}")
+        mydb.commit()
+        cursor.close()
+
+        return None
+
+    def get_all(self):
+        cursor = mydb.cursor()
+        cursor.execute('SELECT * FROM users')
+        rows = cursor.fetchall()
+
+        # Display the results
+        for row in rows:
+            user_id, username = row
+            print(f"id : {user_id} - full name : {username}")
+        # Close the cursor and connection
+        cursor.close()
+
+        return None
+
+    def get_users_contact_list(self, user_id:int):
+        cursor = mydb.cursor()
+        cursor.execute(f"SELECT first_name,last_name,number,address FROM contacts WHERE user_id = {user_id}")
+        rows = cursor.fetchall()
+
+        # Display the results
+        for row in rows:
+            first_name,last_name,number,address = row
+            print(f"First Name : {first_name} - Last Name : {last_name} - Address : {address} - Number : {number}")
+        # Close the cursor and connection
+        cursor.close()
+
+        return None
+
 
 ######### Methods execution ##########
-get_all()
 
+# get_all()
+#
 user = User()
-user.get_a_user(1)
-user.get_a_user(45)
+# user.create_new_user("Kathrin Hill")
+# user.get_all()
+#
+# user.get_a_user(1)
+# user.get_a_user(45)
+#
+# user.create_new_user("George Orwell")
+# user.update_username(1,"Kathrin Hilton")
+# user.delete_user(2)
+#
+user.get_users_contact_list(1)
 
-user.create_new_user("Kathrin Hill")
+#get_users_and_contacts()
+
+
 
 
 
